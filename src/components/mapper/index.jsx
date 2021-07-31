6999b0fda8269af2f008862818daf562792f7c91
Alejandro Emilio Matute Rojas <amatute@baufest.com>
2021-07-31 03:28:01 -0300

    lista completamente mapeada y reducida a 4 items

--- src/components/mapper/index.jsx
+++ src/components/mapper/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {ListGroup, Image, Container} from 'react-bootstrap';
+import shippingImage from '../../assets/ic_shipping.png';
 
 function List(data) {
     let mapping = data.slice(0, 4).map(items => {
+        let shipping = items.shipping.free_shipping === true ? <Image src={shippingImage} rounded /> : null;
+        let amount = items.price.toLocaleString(2);
+        let currency = items.currency_id === "ARS" ? "$" : "USD$";
+        let city = items.address.state_name;
+        let conditions = items.condition;
+        let itemTitle = items.title;
+        let thumbnailUrl = items.thumbnail;
         return(
             <ListGroup.Item>
-                <Image className="thumbnail-fit" src={items.thumbnail} thumbnail />
+                <Image className="thumbnail-fit" src={thumbnailUrl} thumbnail />
+                <div>{currency}{amount}</div>
+                <label>{itemTitle}</label>
+                <label>{conditions}</label>
+                {shipping}
+                <label>{city}</label>
             </ListGroup.Item>
         )
     });
@@ -13,7 +26,6 @@ function List(data) {
 }
 
 const ResultsItems = props => {
-    console.log('todos los resultados:',props);
     const listOfItems = props.location.state;
     return(
         <>
